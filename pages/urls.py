101a57d82277b1a1315bb1e69b9975184a14dfcd
Seraj Safarian <70447169+serajsfr@users.noreply.github.com>
2024-07-03 19:49:10 +0330

    Login
    Login created

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('edit-projects/<int:project_id>/', views.edit_projects, name='edit_projects'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,33 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Project, Skill
 from .forms import ProjectForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 def home(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+    else:
+        form = AuthenticationForm()
+
     projects = Project.objects.all()
     skills = Skill.objects.all()
-    return render(request, "pages/home.html", {'projects': projects, 'skills': skills})
+    context = {
+        'form': form,
+        'projects': projects,
+        'skills': skills,
+    }
+    return render(request, 'pages/home.html', context)
 
+@login_required
 def edit_projects(request, project_id):
     project = get_object_or_404(Project, id=project_id)
     
