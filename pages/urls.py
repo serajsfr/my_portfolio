c90909e26101a41ff826e1b081e221c7a33b385a
Seraj Safarian <70447169+serajsfr@users.noreply.github.com>
2024-07-03 18:44:40 +0330

    Update home.html
    Projects are editable

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name='home'),
+    path('', views.home, name='home'),
+    path('edit-projects/<int:project_id>/', views.edit_projects, name='edit_projects'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Project, Skill
+from .forms import ProjectForm
 
 def home(request):
     projects = Project.objects.all()
     skills = Skill.objects.all()
     return render(request, "pages/home.html", {'projects': projects, 'skills': skills})
+
+def edit_projects(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    
+    if request.method == 'POST':
+        form = ProjectForm(request.POST, instance=project)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = ProjectForm(instance=project)
+    
+    return render(request, "pages/edit_projects.html", {'form': form})
